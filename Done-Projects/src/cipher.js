8e9e97d80afc76d97ae627113d3b1175eef44818
skinhead12 <86309154+skinhead12@users.noreply.github.com>
2021-07-08 20:09:51 -0500

    cambios realizados a diseño y funciones

--- Done-Projects/src/cipher.js
+++ Done-Projects/src/cipher.js
@@ -1,10 +1,51 @@
 
 
 const cipher = {
-  // ...
+    code: function (texto,offset){
+        
+       
+        let offsetNumber = Number(offset); // creamos un variable para colocar a offset como entero
+        let caja = []; // creamos un arreglo vacio para guardar los datos de texto ya codificado
+        
+        for (let i=0; i<texto.length ; ++i) { // se hace un for para recorrer la cadena de texto y saber la longitud
+        
+        let codificar = ((texto.charCodeAt(i))); // transformar texto a ascii toda la cadena, como se ingresa texto como string se pasa a entero
+        let resultadoCode = (codificar + offsetNumber); // se suma el numero ascii y el desplazamiento
+        let resultadoFinal = (String.fromCharCode(resultadoCode)); // imnprimir resultado ascii a abecedario
+        caja.push(resultadoFinal); // le decimos al arreglo que ordene los valores conforme fueron entrando 
+        
+       
+        }
+        return caja;
+
+        },
+
+    decode: function(texto2,offset2){
+
+        let offsetNumber2 = Number(offset2); // creamos un variable para colocar a offset como entero
+        let caja2 = []; // creamos un arreglo vacio para guardar los datos de texto ya codificado
+
+        for (let i=0; i<texto2.length ; ++i) { // se hace un for para recorrer la cadena de texto y saber la longitud
+
+        let decodificar = (texto2.charCodeAt(i)); // transformar de texto a ascii
+        let resultadoDecode = (decodificar - offsetNumber2); // se resta el numero ascii y el desplazamiento
+        let resultado = (String.fromCharCode(resultadoDecode)); // se tranforma de ascii a adecedario
+        caja2.push(resultado); // guarda resultado en el arreglo caja2 y lo acomoda 
+}
+        return caja2;
+  },
+
+    alerta: function(){
+        alert('Instrucciones:\nIngresa un valor numerico a N, este sera el numero de desplazamiento que usara tu codigo.\nNo dejes espacios en blanco mas que el ultimo recuadro donde imprimira el resultado.');
+    },
+
+    borrar: function(){
+      btnClear();
+
+    }
 };
 
 export default cipher;
-// funcion de cifrar y decifrar?
+
 
 
--- Done-Projects/src/index.js
+++ Done-Projects/src/index.js
@@ -1,6 +1,34 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const btnCifrar = document.getElementById('btnCodifica'); // evento del boton codifica 
+btnCifrar.addEventListener ('click', function(){
+const texto = document.getElementById('boxOne').value; // tomar valores de boxone para codificar
+let offset = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
+const caja = cipher.code(texto, offset);  // creamos un arreglo 
+document.getElementById('boxTwo').value = caja.join(''); // imprimir resultado en el textarea boxTwo
+    cipher.code(texto);
+    });
 
+const btnDecifrar = document.getElementById('btnDecodifica'); // evento del boton decodifica
+btnDecifrar.addEventListener('click', function(){
+const texto2 = document.getElementById('boxOne').value; // tomar valores de boxone para decodificar
+let offset2 = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
+const caja2 = cipher.decode(texto2, offset2);
+document.getElementById('boxTwo').value = caja2.join(''); // imprimir resultado en el textarea boxTwo
+cipher.decode(texto2);    
+    });    
+
+const btnAlerta = document.getElementById('btnInstructions');
+btnAlerta.addEventListener('click', function(){
+cipher.alerta();    
+});
+
+const btnClear = document.getElementById('btnBorrar'); // verificar boton borrar
+btnClear.addEventListener('click', function(){
+document.getElementById('boxOne').value = '';
+document.getElementById('BoxN').value = '';
+document.getElementById('boxTwo').value = '';
+cipher.borrar();    
+});
 
 
