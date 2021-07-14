259b8da5ebe5c4ced7f3f30bc8e3ff2c61fe69c5
Edith Resenos <edithr_isc@hotmail.com>
2021-07-14 17:17:44 -0500

    subiendo cliclos if a proyecto

--- Done-Projects/src/cipher.js
+++ Done-Projects/src/cipher.js
@@ -9,31 +9,65 @@ const cipher = {
         
         for (let i=0; i<texto.length ; ++i) { // se hace un for para recorrer la cadena de texto y saber la longitud
         
-        let codificar = ((texto.charCodeAt(i))); // transformar texto a ascii toda la cadena, como se ingresa texto como string se pasa a entero
-        let resultadoCode = (codificar + offsetNumber); // se suma el numero ascii y el desplazamiento
-        let resultadoFinal = (String.fromCharCode(resultadoCode)); // imnprimir resultado ascii a abecedario
-        caja.push(resultadoFinal); // le decimos al arreglo que ordene los valores conforme fueron entrando 
+        let codificarAscii = ((texto.charCodeAt(i))); // transformar texto a ascii toda la cadena, como se ingresa texto como string se pasa a entero
         
-       
+        // creamos una condiiconal para colocar un rango
+        // si el texto codificado es menor o igual a 65 y menor o igual a 90 
+        if (codificarAscii >= 65 && codificarAscii <= 90){
+        // creamos posicion en base a la formula cesar ((x - 65 + n) % 26 + 65) mayusculas        
+            let posicion = (codificarAscii - 65 + offsetNumber) % 26 + 65;
+            let mensajeCodificado = (String.fromCharCode(posicion));
+            caja.push(mensajeCodificado);
+          
+        // creamos posicion en base a la formula cesar ((x - 97 + n) % 26 + 97) minusculas
+        } else if   (codificarAscii >= 97 && codificarAscii <= 122){
+            let posicion = (codificarAscii - 97 + offsetNumber) % 26 + 97; 
+            let mensajeCodificado = (String.fromCharCode(posicion));
+            caja.push(mensajeCodificado);
+
+
+        } else {
+            let resultadoCode = (codificarAscii + offsetNumber); // se suma el numero ascii y el desplazamiento
+            let ResultadoFinal = (String.fromCharCode(resultadoCode)); // imnprimir resultado ascii a abecedario
+            caja.push(ResultadoFinal); // le decimos al arreglo que ordene los valores conforme fueron entrando
+        }
         }
         return caja;
 
         },
 
-    decode: function(texto2,offset2){
+    decode: function(texto,offset){
 
-        let offsetNumber2 = Number(offset2); // creamos un variable para colocar a offset como entero
-        let caja2 = []; // creamos un arreglo vacio para guardar los datos de texto ya codificado
+        let offsetNumber = Number(offset); // creamos un variable para colocar a offset como entero
+        let caja = []; // creamos un arreglo vacio para guardar los datos de texto ya codificado
 
-        for (let i=0; i<texto2.length ; ++i) { // se hace un for para recorrer la cadena de texto y saber la longitud
+        for (let i=0; i<texto.length ; ++i) { // se hace un for para recorrer la cadena de texto y saber la longitud
 
-        let decodificar = (texto2.charCodeAt(i)); // transformar de texto a ascii
-        let resultadoDecode = (decodificar - offsetNumber2); // se resta el numero ascii y el desplazamiento
-        let resultado = (String.fromCharCode(resultadoDecode)); // se tranforma de ascii a adecedario
-        caja2.push(resultado); // guarda resultado en el arreglo caja2 y lo acomoda 
-}
-        return caja2;
-  },
+            let codificarAscii = ((texto.charCodeAt(i))); // transformar texto a ascii toda la cadena, como se ingresa texto como string se pasa a entero
+        
+            // creamos una condiiconal para colocar un rango
+            // si el texto codificado es menor o igual a 65 y menor o igual a 90 
+            if (codificarAscii >= 65 && codificarAscii <= 90){
+             // creamos posicion en base a la formula cesar ((x + 65 - n) % 26 + 65) mayusculas           
+                let posicion = (codificarAscii + 65 - offsetNumber) % 26 + 65;
+                let mensajeCodificado = (String.fromCharCode(posicion));
+                caja.push(mensajeCodificado);
+              
+        // creamos posicion en base a la formula cesar ((x + 97 - n) % 26 + 97) minusculas
+            } else if   (codificarAscii >= 97 && codificarAscii <= 122){
+                let posicion = (codificarAscii + 97 - offsetNumber) % 26 + 97;
+                let mensajeCodificado = (String.fromCharCode(posicion));
+                caja.push(mensajeCodificado);
+               
+    
+            } else {
+                let resultadoCode = (codificarAscii - offsetNumber); // se suma el numero ascii y el desplazamiento
+                let ResultadoFinal = (String.fromCharCode(resultadoCode)); // imnprimir resultado ascii a abecedario
+                caja.push(ResultadoFinal); // le decimos al arreglo que ordene los valores conforme fueron entrando
+            }
+    }
+        return caja;
+    },
 
     alerta: function(){
         alert('Instrucciones:\nIngresa un valor numerico a N, este sera el numero de desplazamiento que usara tu codigo.\nNo dejes espacios en blanco mas que el ultimo recuadro donde imprimira el resultado.');
--- Done-Projects/src/index.js
+++ Done-Projects/src/index.js
@@ -1,29 +1,32 @@
 import cipher from './cipher.js';
 
-const btnCifrar = document.getElementById('btnCodifica'); // evento del boton codifica 
-btnCifrar.addEventListener ('click', function(){
+const btnCifrar = document.getElementById('btnCodifica'); // boton codifica
+const btnDecifrar = document.getElementById('btnDecodifica'); // boton decodifica
+const btnAlerta = document.getElementById('btnInstructions'); // boton instrucciones
+const clear = document.getElementById('btnBorrar'); // boton borrar 
+
+  // evento boton cifrar
+btnCifrar.addEventListener ('click', function(){    
 const texto = document.getElementById('boxOne').value; // tomar valores de boxone para codificar
-let offset = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
+const offset = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
 const caja = cipher.encode(texto, offset);  // creamos un arreglo 
 document.getElementById('boxTwo').value = caja.join(''); // imprimir resultado en el textarea boxTwo
-    cipher.encode(texto);
     });
 
-const btnDecifrar = document.getElementById('btnDecodifica'); // evento del boton decodifica
+    // evento boton decifrar
 btnDecifrar.addEventListener('click', function(){
-const texto2 = document.getElementById('boxOne').value; // tomar valores de boxone para decodificar
-let offset2 = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
-const caja2 = cipher.decode(texto2, offset2);
-document.getElementById('boxTwo').value = caja2.join(''); // imprimir resultado en el textarea boxTwo
-cipher.decode(texto2);    
-    });    
+const texto = document.getElementById('boxOne').value; // tomar valores de boxone para codificar
+const offset = document.getElementById('BoxN').value;  // tomar valores del BoxN rango del desplazamiento
+let caja = cipher.decode(texto, offset);
+document.getElementById('boxTwo').value = caja.join(''); // imprimir resultado en el textarea boxTwo 
+ });    
 
-const btnAlerta = document.getElementById('btnInstructions');
+// boton instrucciones
 btnAlerta.addEventListener('click', function(){
 cipher.alerta();    
 });
 
-const clear = document.getElementById('btnBorrar');
+// boton borrar
 clear.addEventListener('click', function() {
     document.getElementById('boxOne').value = '';
     document.getElementById('BoxN').value = '';
